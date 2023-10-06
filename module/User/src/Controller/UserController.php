d295fd6a46bfece10c548239025d7fe9865e91d5
aldeirnohan <aldeir_nohan03@live.com>
2023-10-06 19:33:22 -0300

    controllers done

--- module/User/src/Controller/UserController.php
+++ module/User/src/Controller/UserController.php
@@ -7,7 +7,7 @@ namespace User\Controller;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\ViewModel;
 use User\Entity\User;
-
+use User\Form\UserForm;
 
 class UserController extends AbstractActionController
 {
@@ -40,14 +40,86 @@ class UserController extends AbstractActionController
     }
 
     public function addAction(){
-        return new ViewModel();
+        $form = new UserForm();
+        
+        $request = $this->getRequest();
+        
+        if ($request->isPost()) {
+            $user = new User();
+            $form->setInputFilter($user->getInputFilter());
+            $form->setData($request->getPost());
+            
+            if ($form->isValid()) {
+                $user->exchangeArray($form->getData());
+                $this->entityManager->persist($user);
+                $this->entityManager->flush();
+                
+                return $this->redirect()->toRoute('user');
+            }
+        }
+        
+        return new ViewModel([
+            'form' => $form,
+        ]);
     }
 
     public function editAction(){
-        return new ViewModel();
+        $id = (int) $this->params()->fromRoute('id', 0);
+        
+        if ($id === 0) {
+            return $this->redirect()->toRoute('user');
+        }
+        
+        $user = $this->entityManager->getRepository(User::class)->find($id);
+        
+        if ($user === null) {
+            return $this->redirect()->toRoute('user');
+        }
+        
+        $form = new UserForm();
+        $form->bind($user);
+        
+        $request = $this->getRequest();
+        
+        if ($request->isPost()) {
+            $form->setInputFilter($user->getInputFilter());
+            $form->setData($request->getPost());
+            
+            if ($form->isValid()) {
+                $this->entityManager->flush();
+                
+                return $this->redirect()->toRoute('user');
+            }
+        }
+        
+        return new ViewModel([
+            'user' => $user,
+            'form' => $form,
+        ]);
     }
 
     public function removeAction(){
-        return new ViewModel();
+        $id = (int) $this->params()->fromRoute('id', 0);
+
+        if ($id === 0) {
+            return $this->redirect()->toRoute('home');
+        }
+
+        $user = $this->entityManager->getRepository(User::class)->find($id);
+
+        if ($user === null) {
+            return $this->redirect()->toRoute('home');
+        }
+
+        if ($this->getRequest()->isPost()) {
+            $this->entityManager->remove($user);
+            $this->entityManager->flush();
+
+            return $this->redirect()->toRoute('home');
+        }
+
+        return new ViewModel([
+            'id' => $id,
+        ]);
     }
 }
